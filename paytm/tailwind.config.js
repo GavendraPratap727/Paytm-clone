4087ffe3b5d8a55ace6e29a0ab91d4763a6638b8
Gavendra <pratapgavendra01@gmail.com>
2025-07-02 11:31:05 +0530

    commit

---
+++ paytm/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        nilla:"#00BAF2",
+        darknilla:"#002970",
+        lightcolor:"#f5f5f5",
+        buttoncolornila:"#00AFE3",
+        lightnilla:"#0f4a8a",
+        greybackground:"#f5f7fa",
+        ultralightnilla:"#4ccff6",
+      }
+    },
+  },
+  plugins: [],
+}
